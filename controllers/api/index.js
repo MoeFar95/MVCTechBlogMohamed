2ae680ecafde0156058e2104a0d22a1a0fb4fcaf
MoeFar95 <89990024+MoeFar95@users.noreply.github.com>
2021-12-10 19:24:30 -0500

    push

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const userRoutes = require("./userroutes");
 const postRoutes = require("./postroutes");
 
-router.use("/user", userroutes);
-router.use("/post", postroutes);
+router.use("/user", userRoutes);
+router.use("/post", postRoutes);
 
 module.exports = router;
